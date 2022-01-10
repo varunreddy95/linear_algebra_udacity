c12392ea7b91803b8325757b7cf8399cb0792e8b
kamathams0 <s6sakama@uni-bonn.de>
2022-01-10 22:53:01 +0100

    First class of development

---
+++ src/Simulator.java
@@ -0,0 +1,8 @@
+import java.lang.*;
+import java.util.*;
+
+class Simulator{
+    public static void main(String[] args){
+        System.out.println("This is F1 strategy simulation");
+    }
+}        
